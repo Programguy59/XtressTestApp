4880a7c90b01633cf0cd67c9c2872258201ecc8e
Magnus Melgaard Jonge <magn8244@elev.techcollege.dk>
2024-11-06 09:11:03 +0100

    Magnus #1

---
+++ Element.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XtressTestApp
+{
+    public class Element
+    {
+        int Value;
+        Element NextElement;
+        public Element(int value, Element nextElement)
+        {
+            Value = value;
+            NextElement = nextElement;
+        }
+        
+    }
+
+    
+}
---
+++ OurLinkList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XtressTestApp
+{
+    public class OurLinkList
+    {
+        Element FirstElement;
+        public OurLinkList(Element firstElement) 
+        {
+            FirstElement = firstElement;
+        }
+        
+
+        public void Add_First(Element data)
+        {
+
+        }
+        public void Remove_First()
+        {
+
+        }
+
+        public int Count()
+        {
+            return 2;
+        }
+        
+        public string To_String()
+        {
+            return "Hello wrld";
+        }
+
+        public void Sort()
+        {
+
+        }
+    }
+}
